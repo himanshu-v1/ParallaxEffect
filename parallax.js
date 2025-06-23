c5b7fd8c9459b04a884593c48efcbf376039490a
Himanshu Sharma <Himanshu.st6@gmail.com>
2025-06-23 20:42:33 +0530

    Update parallax.js
    1. Added update to support multiple parallax components on page
    2. Added handling to identify color from next component and apply instead of default white.

--- parallax.js
+++ parallax.js
@@ -1,25 +1,85 @@
+// Return next element if it is not a parallax element to apply background color
+function isNextNonParallax(el) {
+  const rawComponents = document.querySelectorAll('[data-component]');    // identifier for each component, getting all components on page
+  const components = Array.from(rawComponents).filter((i) => {
+    if (i.dataset.component !== 'parallax' && i.closest('.parallax')) return false;    // eliminate nested components
+    else return true;
+  });
+  const curEl = el;
+  const curElIndex = Array.from(components).indexOf(curEl);
+  const nextEl = components[curElIndex + 1];
+  let isNextParallax = '';
+
+  if (nextEl && nextEl.classList.contains('parallax')) {
+    isNextParallax = '';
+  } else {
+    isNextParallax = nextEl;
+  }
+
+  return isNextParallax;
+};
+
+function findAncestor(el, sel1, sel2) {
+  if (el.classList.contains(sel1) || el.classList.contains(sel2)) {
+      return el;
+  }
+  while ((el = el.parentElement) && (!el.matches(`[class*="${sel1}"]`) || !el.matches(`[class*="${sel2}"]`)));
+  return el;
+};
+
+// Find background color for next component
+function findBGColor(el) {
+  if (getComputedStyle(el).backgroundColor !== 'rgba(0, 0, 0, 0)') {
+    return getComputedStyle(el).backgroundColor;
+  } else {
+    // theme class
+    const legacyThemeEl = findAncestor(el, 'theme__', 'custom-theme');
+    if (legacyThemeEl) {
+      return getComputedStyle(legacyThemeEl).backgroundColor;
+    }
+  }
+
+  return 'white'; // Fallback color
+};
+
 function parallax() {
     const el = document.querySelectorAll('.parallax');
 
-    const _bg = document.createElement('div');
-    _bg.classList.add('parallax-overlay');
-    el[el.length - 1].querySelector('parllax-image').appendChild(_bg);
+    // handling to compensate for no default background color for components and uneven z-index
+    el.forEach((item) => {
+        const nextEl = isNextNonParallax(item);
+        if (nextEl) {
+            const bgColor = findBGColor(nextEl);
+            const colorBg = document.createElement('div');
+            colorBg.classList.add('parallax-overlay');
+            colorBg.style.backgroundColor = bgColor;
+            if (!item.querySelector('.parallax-overlay')) {
+                item.querySelector('.cmp-container-image').appendChild(colorBg);
+            }
+        }
+    });
 
+    // handling scroll below (fliexibility limitations when using IntersectionObserver)
     window.addEventListener('scroll', () => {
-        el.forEach((e, idx) => {
+        el.forEach((e) => {
             const rect = e.getBoundingClientRect();
             const image = e.querySelector('.parallax-image');
+            const colorBg = item.querySelector('.parallax-overlay');
 
             if(rect.top <= 0) {
                 const contentDivBottom = e.querySelector('.parallax-content').getBoundingClientRect().bottom;
                 $('.parallax .fixed').removeClass('fixed');
 
-                contentDivBottom > parseInt(-(window.innerHeight / 3.6)) ? 
-                    image.classList.add('fixed') : image.classList.remove('fixed');
+                const isAtPosition = contentDiv > parseInt(-(window.innerHeight / 3.6));
+                image.classList.toggle('fixed', isAtPosition);
 
-                if(rect.bottom <= window.innerHeight && idx === el.length-1) {
-                    const h = window.innerHeight - rect.bottom;
-                    e.querySelector('.parallax-overlay') && (e.querySelector('.parallax-overlay').style.height = `${h}px`);
+                if (colorBg) {
+                    if(rect.bottom <= window.innerHeight) {
+                        const h = window.innerHeight - rect.bottom;
+                        colorBg.style.height = `${h}px`;
+                    } else {
+                        colorBg.style.height = '0px';
+                    }
                 }
             } else {
                 image.classList.remove('fixed');
